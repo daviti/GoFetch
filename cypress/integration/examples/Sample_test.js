d1368f2d0c584970470a6817975bfb1eac99dcb4
David Ortiz <ortizcdavid@gmail.com>
2021-01-12 13:42:34 -0800

    update test with message

--- cypress/integration/examples/Sample_test.js
+++ cypress/integration/examples/Sample_test.js
@@ -1,13 +1,21 @@
 //search gofetch on google
 describe('My First Test', () => {
     beforeEach(() => {
-    cy.visit('https://www.google.com')
+    cy.wait(1000)
+    //open goole.com
+    cy.visit('https://google.com')
+    cy.wait(1000)
 })
-    it('Gets, types and asserts - type into a DOM element', () => {
+    //assert what will be used to search, inout, select, send
+    it('Get, type and assert - type into a DOM element', () => {
+    //search for the element
     cy.get('.a4bIc').type('GoFetch Health: home')
+    //Select the search
     cy.contains('.sbl1').click()
     cy.wait(1000)
+    //Selects link from search
     cy.get('.TbwUpd.NJjxre')
+    //Homepage opens
     cy.contains('GoFetch Health: Home').click()
 
   })
--- cypress/integration/examples/window.spec.js
+++ cypress/integration/examples/window.spec.js
@@ -3,20 +3,24 @@
 context('GoFetch', () => {
   beforeEach(() => {
     cy.visit('https://gofetch.ca')
+    cy.wait(1000)
   })
 
   it('cy.window() - get the global window object', () => {
     // Verifying for the whole framework page
     cy.window().should('have.property', 'top')
+    cy.wait(1000)
   })
 
   it('cy.document() - get the document object', () => {
     // verifying that the docuemnt object is present
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy.wait(1000)
   })
 
   it('cy.title() - get the title', () => {
     /// gets title from homepage
     cy.title().should('include', 'GoFetch')
+    cy.wait(1000)
   })
 })
